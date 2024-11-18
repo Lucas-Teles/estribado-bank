d68b1135556fbe8984863adbf66d336a2f4aaa17
Lucas Teles <lucasouzateles@gmail.com>
2024-11-18 15:46:42 -0300

    refactor: refatorando o código

--- src/main/java/br/com/estribadobank/banco/controller/ClienteController.java
+++ src/main/java/br/com/estribadobank/banco/controller/ClienteController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -59,7 +58,6 @@ public class ClienteController {
     }
 
     @PostMapping("/logout")
-<<<<<<< HEAD
     public ResponseEntity<Object> logoutCliente(@RequestBody Map<String, String> loginBody) {
         try {
             String cpf = loginBody.get("cpf");
@@ -76,73 +74,70 @@ public class ClienteController {
             }
             clienteService.deslogarCliente(cliente);
             return ResponseEntity.ok("Cliente deslogado com sucesso");
-=======
-    public ResponseEntity<Object> logoutCliente(@RequestBody Map<String, String> logoutBody) {
-        try {
-            Cliente cliente = clienteRepository.findClienteByCpfAndSenha(logoutBody.get("cpf"), logoutBody.get("senha"));
-            if (cliente != null){
-                clienteService.deslogarCliente(cliente);
-                clienteRepository.save(cliente);
-                return ResponseEntity.ok("Cliente deslogado com sucesso");
-            } else {
-                throw new ClienteException.LoginIncorretoException();
+            public ResponseEntity<Object> logoutCliente (@RequestBody Map < String, String > logoutBody){
+                try {
+                    Cliente cliente = clienteRepository.findClienteByCpfAndSenha(logoutBody.get("cpf"), logoutBody.get("senha"));
+                    if (cliente != null) {
+                        clienteService.deslogarCliente(cliente);
+                        clienteRepository.save(cliente);
+                        return ResponseEntity.ok("Cliente deslogado com sucesso");
+                    } else {
+                        throw new ClienteException.LoginIncorretoException();
+                    }
+                } catch (ClienteException.ClienteNaoCadastradoException e) {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+                } catch (Exception e) {
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao realizar o logout");
+                }
             }
->>>>>>> 1ca7db0192e7c303ccf87e5c8a721631fea2a3b7
-        } catch (ClienteException.ClienteNaoCadastradoException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao realizar o logout");
+
+            @PutMapping("/{id}/atualizar")
+            public ResponseEntity<Object> atualizarCliente (@PathVariable UUID id, @RequestBody Cliente cliente){
+                try {
+                    clienteService.atualizarCliente(id, cliente);
+                    return ResponseEntity.ok("Cliente atualizado com sucesso");
+                } catch (ClienteException.ClienteNaoCadastradoException | ClienteException.ClienteNaoEstaLogado e) {
+                    return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+                } catch (Exception e) {
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar o cliente");
+                }
+            }
+
+            @PatchMapping("/{idCliente}/mudar-senha")
+            public ResponseEntity<Object> mudarSenha (@RequestBody Map < String, String > novaSenhaBody){
+                try {
+                    Cliente cliente = clienteRepository.findClienteByCpfAndEmail(novaSenhaBody.get("cpf"), novaSenhaBody.get("email"));
+                    clienteService.mudarSenha(cliente, novaSenhaBody.get("senha"));
+                    return ResponseEntity.ok().build();
+                } catch (ClienteException.ClienteNaoCadastradoException e) {
+                    return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+                }
+            }
+
+
+            @DeleteMapping("/{id}/encerrar-conta")
+            public ResponseEntity<Object> removerCliente (@PathVariable UUID id){
+                try {
+                    clienteService.removerCliente(id);
+                    return ResponseEntity.ok("Cliente removido com sucesso");
+                } catch (ClienteException.ClienteNaoCadastradoException | ClienteException.ClienteNaoEstaLogado e) {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+                } catch (Exception e) {
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao remover o cliente");
+                }
+            }
+
+            @PostMapping("/{id}/upgrade-conta")
+            public ResponseEntity<Object> upgradeDaConta (@PathVariable UUID id){
+                try {
+                    clienteService.upgradeDaConta(id);
+                    return ResponseEntity.ok("Conta atualizada com sucesso");
+                } catch (ClienteException.ClienteNaoCadastradoException | ClienteException.ClienteNaoEstaLogado |
+                         ClienteException.RendaMinimaException e) {
+                    return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+                } catch (Exception e) {
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar a conta");
+                }
+            }
+
         }
-    }
-
-    @PutMapping("/{id}/atualizar")
-    public ResponseEntity<Object> atualizarCliente(@PathVariable UUID id, @RequestBody Cliente cliente) {
-        try {
-            clienteService.atualizarCliente(id, cliente);
-            return ResponseEntity.ok("Cliente atualizado com sucesso");
-        } catch (ClienteException.ClienteNaoCadastradoException | ClienteException.ClienteNaoEstaLogado e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar o cliente");
-        }
-    }
-
-    @PatchMapping("/{idCliente}/mudar-senha")
-    public ResponseEntity<Object> mudarSenha(@RequestBody Map<String, String> novaSenhaBody){
-        try {
-            Cliente cliente = clienteRepository.findClienteByCpfAndEmail(novaSenhaBody.get("cpf"), novaSenhaBody.get("email"));
-            clienteService.mudarSenha(cliente, novaSenhaBody.get("senha"));
-            return ResponseEntity.ok().build();
-        } catch (ClienteException.ClienteNaoCadastradoException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
-    }
-
-
-
-    @DeleteMapping("/{id}/encerrar-conta")
-    public ResponseEntity<Object> removerCliente(@PathVariable UUID id) {
-        try {
-            clienteService.removerCliente(id);
-            return ResponseEntity.ok("Cliente removido com sucesso");
-        } catch (ClienteException.ClienteNaoCadastradoException | ClienteException.ClienteNaoEstaLogado e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao remover o cliente");
-        }
-    }
-
-    @PostMapping("/{id}/upgrade-conta")
-    public ResponseEntity<Object> upgradeDaConta(@PathVariable UUID id) {
-        try {
-            clienteService.upgradeDaConta(id);
-            return ResponseEntity.ok("Conta atualizada com sucesso");
-        } catch (ClienteException.ClienteNaoCadastradoException | ClienteException.ClienteNaoEstaLogado |
-                 ClienteException.RendaMinimaException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar a conta");
-        }
-    }
-
-}
--- src/main/java/br/com/estribadobank/banco/service/ClienteService.java
+++ src/main/java/br/com/estribadobank/banco/service/ClienteService.java
@@ -8,9 +8,9 @@ import br.com.estribadobank.banco.model.entity.ContaPagamento;
 import br.com.estribadobank.banco.repository.ClienteRepository;
 import br.com.estribadobank.banco.repository.ContaRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -23,27 +23,31 @@ public class ClienteService {
         this.clienteRepository = clienteRepository;
     }
 
-    public Cliente cadastrarCliente(Cliente cliente){
-        boolean clienteEstaCadastrado = clienteRepository.existsByNomeAndCpf(cliente.getNome(), cliente.getCpf());
-        if (clienteEstaCadastrado){
+    public Cliente cadastrarCliente(Cliente cliente) {
+        boolean clienteCadastrado = clienteRepository.existsByNomeAndCpf(cliente.getNome(), cliente.getCpf());
+        if (clienteCadastrado) {
             throw new ClienteException.ClienteJaExisteException();
         }
         return clienteRepository.save(cliente);
     }
 
-    public Conta selecionarConta(Cliente cliente){
-        if (cliente.getRendaMensal().compareTo(new BigDecimal("2118.00")) >= 0){
+    public Conta selecionarConta(Cliente cliente) {
+        if (cliente.getRendaMensal().compareTo(new BigDecimal("2118.00")) >= 0) {
             return new ContaCorrente(cliente);
         }
         return new ContaPagamento(cliente);
     }
 
-    public void removerConta(Cliente cliente){
+    public void removerConta(Cliente cliente) {
         Conta conta = contaRepository.findByCliente(cliente);
-        contaRepository.delete(conta);
+        if (conta != null){
+            contaRepository.delete(conta);
+        } else {
+            throw new ClienteException.ClienteNaoCadastradoException();
+        }
     }
 
-    public void removerCliente(UUID clienteId){
+    public void removerCliente(UUID clienteId) {
         Cliente cliente = clienteRepository.findById(clienteId)
                 .orElseThrow(ClienteException.ClienteNaoCadastradoException::new);
 
@@ -56,52 +60,25 @@ public class ClienteService {
     }
 
     public void atualizarCliente(UUID id, Cliente cliente) {
-<<<<<<< HEAD
-        Optional<Cliente> clienteExistenteOptional = clienteRepository.findById(id);
+        Cliente clienteExistente = clienteRepository.findById(id)
+                .orElseThrow(ClienteException.ClienteNaoCadastradoException::new);
 
-        if (clienteExistenteOptional.isPresent()) {
-            Cliente clienteExistente = clienteExistenteOptional.get();
-
-            if (cliente.isLogado()) {
-                // Copiar os atributos do cliente para o cliente existente
-                clienteExistente.setTelefone(cliente.getTelefone());
-                clienteExistente.setEndereco(cliente.getEndereco());
-                clienteExistente.setRendaMensal(cliente.getRendaMensal());
-                clienteExistente.setEmail(cliente.getEmail());
-                clienteExistente.setSenha(cliente.getSenha());
-
-                // Verificar condição de renda para ofertar upgrade
-                if (cliente.getRendaMensal().compareTo(new BigDecimal("2118.00")) >= 0) {
-                    ofertarUpgradeDaConta(clienteExistente);
-                }
-
-
-                clienteRepository.save(clienteExistente);
-=======
-        if (clienteRepository.findById(id).isPresent()) {
-            Cliente clienteAtualizado = clienteRepository.findById(id).get();
-            if (clienteAtualizado.isLogado()) {
-                clienteAtualizado.setTelefone(cliente.getTelefone());
-                clienteAtualizado.setEndereco(cliente.getEndereco());
-                clienteAtualizado.setRendaMensal(cliente.getRendaMensal());
-                clienteAtualizado.setEmail(cliente.getEmail());
-                clienteAtualizado.setSenha(cliente.getSenha());
-                if (clienteAtualizado.getRendaMensal().compareTo(new BigDecimal("2118.00")) >= 0){
-                    ofertarUpgradeDaConta(clienteAtualizado);
-                    clienteRepository.save(clienteAtualizado);
-                }
-                clienteRepository.save(clienteAtualizado);
->>>>>>> 1ca7db0192e7c303ccf87e5c8a721631fea2a3b7
-            } else {
-                throw new ClienteException.ClienteNaoEstaLogado();
-            }
-        } else {
-            throw new ClienteException.ClienteNaoCadastradoException();
+        if (!cliente.isLogado()){
+            throw new ClienteException.ClienteNaoEstaLogado();
         }
+
+        clienteExistente.setTelefone(cliente.getTelefone());
+        clienteExistente.setEndereco(cliente.getEndereco());
+        clienteExistente.setRendaMensal(cliente.getRendaMensal());
+        clienteExistente.setEmail(cliente.getEmail());
+        clienteExistente.setSenha(cliente.getSenha());
+
+        if (cliente.getRendaMensal().compareTo(new BigDecimal("2118.00")) >= 0) ofertarUpgradeDaConta(clienteExistente);
+
+        clienteRepository.save(clienteExistente);
     }
 
-<<<<<<< HEAD
-=======
+
     public void mudarSenha(Cliente cliente, String senha) {
         if (clienteRepository.findById(cliente.getId()).isPresent()) {
             cliente.setSenha(senha);
@@ -110,14 +87,12 @@ public class ClienteService {
             throw new ClienteException.ClienteNaoCadastradoException();
         }
     }
->>>>>>> 1ca7db0192e7c303ccf87e5c8a721631fea2a3b7
 
-    public void ofertarUpgradeDaConta(Cliente cliente){
+    public void ofertarUpgradeDaConta(Cliente cliente) {
         System.out.println("Upgrade de conta disponivel, redirecionar cliente para caso o mesmo queira /upgrade-de-conta");
     }
 
-    @Transactional
-    public void upgradeDaConta(UUID id){
+    public void upgradeDaConta(UUID id) {
         Cliente cliente = clienteRepository.findById(id)
                 .orElseThrow(ClienteException.ClienteNaoCadastradoException::new);
 
@@ -138,8 +113,12 @@ public class ClienteService {
         contaRepository.save(contaNova);
     }
 
-    @Transactional
     public void logarCliente(Cliente cliente) {
+
+        Cliente cliente = clienteRepository.findById(id)
+                .orElseThrow(ClienteException.ClienteNaoCadastradoException::new);
+
+
         if (clienteRepository.findById(cliente.getId()).isPresent()) {
             cliente.setLogado(true);
             clienteRepository.save(cliente);
@@ -148,8 +127,8 @@ public class ClienteService {
         }
     }
 
-    public void deslogarCliente(Cliente cliente){
-        if (clienteRepository.findById(cliente.getId()).isPresent()){
+    public void deslogarCliente(Cliente cliente) {
+        if (clienteRepository.findById(cliente.getId()).isPresent()) {
             cliente.setLogado(false);
             clienteRepository.save(cliente);
         } else {
